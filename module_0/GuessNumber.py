0f5bdffcf99c5d5b4249e4e44d9bc3b3a4203737
Rad <diikaz11@gmail.com>
2020-04-20 17:33:03 +0300

    GN chnge while

--- module_0/GuessNumber.py
+++ module_0/GuessNumber.py
@@ -15,10 +15,18 @@ print ("Загадано число от 1 до 100")
 for count in range(1,101):         # более компактный вариант счетчика
     count = 1
     predict = int((len(range(1,101))/2))
+    predictm = int((len(range(1,101))/2)) + int((len(range(1,101))/4))
+    mredictl = int((len(range(1,101))/2)) - int((len(range(1,101))/4))
     while number != predict:
         count+=1
-        if number > predict: 
-            predict += 1
+        if number > predict and number > predictm: 
+            predictm += 1
+            predict = predictm
+        elif number > predict and number == predictm:
+            predict == predictm
+        elif number > predict and number < predictm:
+            predictm -= 1
+            predict = predictm
         elif number < predict: 
             predict -= 1
     break
